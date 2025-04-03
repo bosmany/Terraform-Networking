fb1233b9af338025e9bcdf306228e60ffba9198a
Bilal Osmany <mohammedbilalosmany@gmail.com>
2025-04-03 10:52:42 +0530

    Create main.tf

---
+++ modules/security/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "web" {
+  name        = "${var.project_name}-web-sg"
+  description = "Allow HTTP/HTTPS traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
