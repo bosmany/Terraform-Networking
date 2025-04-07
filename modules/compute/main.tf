e6481f18b3a8f3651b5d25e2a55f18db9ffdbb54
Bilal Osmany <mohammedbilalosmany@gmail.com>
2025-04-07 13:06:11 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -2,7 +2,5 @@ resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
-
-  tags = { Name = "${var.project_name}-web-server" }
+  vpc_security_group_ids = [var.security_group_id]  # CORRECT argument name
 }
