3527be66c8834531989fec2632f39ad7c7315e4c
Bilal Osmany <mohammedbilalosmany@gmail.com>
2025-04-07 13:07:37 +0530

    Update variable.tf

--- modules/network/variable.tf
+++ modules/network/variable.tf
@@ -1,8 +1,4 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnets" {
-  value = aws_subnet.public[*].id
-}
-
+variable "vpc_cidr" {}
+variable "public_subnets" {}
+variable "azs" {}
+variable "project_name" {}
